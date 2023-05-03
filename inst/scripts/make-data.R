4f293e38b6d1d9e4ee3125d51208bf8c7335115c
Dharmesh Bhuva <dharmesh.bhuva@adelaide.edu.au>
2023-05-03 15:49:23 +0930

    Added metadata file

---
+++ inst/scripts/make-data.R
@@ -0,0 +1,64 @@
+library(SpatialExperiment)
+library(dplyr)
+
+#download data from websites and load as table
+#  - added annotation column where each detection is mapped onto annotations
+#  - annotations are stored in geojson files (see paper for description on how they
+#    were obtained Bhuva et al. 2023)
+
+#xenium
+df_xenium = readRDS('data-local/Xenium_Mar23.rds') |> 
+  dplyr::rename(cell = cell_id, gene = feature_name, x = x_location, y = y_location) |> 
+  mutate(
+    genetype = case_when(
+      grepl('BLANK', gene) ~ 'NegBlank',
+      grepl('NegControlProbe', gene) ~ 'NegPrb',
+      grepl('NegControlCodeword', gene) ~ 'NegCW',
+      TRUE ~ 'Gene'
+    ),
+    cell = ifelse(cell == -1, NA_integer_, cell),
+    counts = 1,
+    technology = 'Xenium'
+  ) |> 
+  dplyr::rename(region = RegionID, level = Level) |> 
+  relocate(sample, cell, gene, genetype, x, y, counts, region, technology, level, Level0:Level11)
+saveRDS(df_xenium, 'data-local/xenium_mm_brain.rds')
+
+#stomics
+df_stomics = readRDS('data-local/STOmics_Brain.rds') |> 
+  select(!UMICount) |> 
+  dplyr::rename(gene = geneID, cell = label, counts = MIDCounts) |> 
+  mutate(technology = 'STOmics', genetype = 'Gene') |> 
+  dplyr::rename(region = RegionID, level = Level) |> 
+  relocate(sample, cell, gene, genetype, x, y, counts, region, technology, level, Level0:Level11)
+saveRDS(df_stomics, 'data-local/stomics_mm_brain.rds')
+
+#cosmx
+df_cosmx = readRDS('data-local/CosMx_NSCLC_tx_annotated.rds') |> 
+  select(!c(sample, x, y)) |> 
+  dplyr::rename(
+    sample = TMA,
+    x = x_global_px,
+    y = y_global_px,
+    gene = target,
+    cell = cell_ID
+  ) |>
+  dplyr::mutate(cell = ifelse(cell == 0, NA, cell), cell = paste(fov, cell, sep = '_')) |>
+  dplyr::select(sample, x, y, gene, cell, fov, Level1, Level2, RegionID) |>
+  mutate(
+    technology = 'CosMx',
+    genetype = ifelse(grepl('NegPrb', gene), 'NegPrb', 'Gene'),
+    cell = ifelse(grepl('NA$', cell), NA_character_, cell),
+    counts = 1
+  ) |> 
+  mutate(RegionID = case_when(
+    RegionID %in% c('Tumour area with stroma', 'Epithelium - panCK+', 'Tumour area') ~ 'Tumour',
+    RegionID %in% c('Abnormal but not tumour') ~ 'Abnormal epithelium',
+    RegionID %in% 'Fibrotic area' ~ 'Fibrotic',
+    RegionID %in% 'Necrotic area' ~ 'Necrotic',
+    RegionID %in% 'Abnormal area' ~ 'Abnormal',
+    TRUE ~ RegionID
+  )) |> 
+  dplyr::rename(region = RegionID) |> 
+  relocate(sample, cell, gene, genetype, x, y, counts, region, technology, Level1:Level2)
+saveRDS(df_cosmx, 'data-local/cosmx_hs_nsclc.rds')
---
+++ inst/scripts/make-metadata.R
@@ -0,0 +1,58 @@
+##build record
+meta = rbind(
+  c(
+    Title = 'xenium_mm_brain',
+    Description = '10x Xenium dataset of 3 serial sections of fresh frozen mouse brain. Raw transcript level data is provided with region annotations for each detection obtained using image registration of the DAPI image to the Allen Brain Atlas. The Aligning Big Brain Atlases (ABBA) plugin in ImageJ was used for image registration.',
+    BiocVersion = as.numeric(as.character(BiocManager::version())) + 0.01,
+    Genome = NA,
+    SourceType = 'CSV',
+    SourceUrl = 'https://www.10xgenomics.com/resources/datasets/fresh-frozen-mouse-brain-replicates-1-standard',
+    SourceVersion = '2023-03-09',
+    Species = 'Mus musculus',
+    TaxonomyId = '10090',
+    Coordinate_1_based = TRUE,
+    DataProvider = '10x',
+    Maintainer = 'Dharmesh D. Bhuva <dharmesh.bhuva@adelaide.edu.au>',
+    RDataClass = 'data.frame',
+    DispatchClass = 'Rds',
+    RDataPath = file.path('SubcellularSpatialData', 'xenium', 'xenium_mm_brain.rds')
+  ),
+  c(
+    Title = 'stomics_mm_brain',
+    Description = 'BGI STOmics dataset of the mouse brain. Raw DNA nanoball spot level data is provided with region annotations for each spot obtained using image registration of the DAPI image to the Allen Brain Atlas. The Aligning Big Brain Atlases (ABBA) plugin in ImageJ was used for image registration.',
+    BiocVersion = as.numeric(as.character(BiocManager::version())) + 0.01,
+    Genome = NA,
+    SourceType = 'TSV',
+    SourceUrl = 'https://ftp.cngb.org/pub/SciRAID/stomics/STDS0000058/Bin1_matrix/Mouse_brain_Adult_GEM_bin1.tsv.gz',
+    SourceVersion = '2023-03-09',
+    Species = 'Mus musculus',
+    TaxonomyId = '10090',
+    Coordinate_1_based = TRUE,
+    DataProvider = 'BGI',
+    Maintainer = 'Dharmesh D. Bhuva <dharmesh.bhuva@adelaide.edu.au>',
+    RDataClass = 'data.frame',
+    DispatchClass = 'Rds',
+    RDataPath = file.path('SubcellularSpatialData', 'stomics', 'stomics_mm_brain.rds')
+  ),
+  c(
+    Title = 'cosmx_hs_nsclc',
+    Description = 'NanoString CosMx dataset of FFPE tissues from 5 non-small cell lung cancer (NSCLC) patients. Lung 5 has 2 serial sections while Lung 9 has 2 non-serial (different location of the tumour) sections from the same patient. Raw transcript level data is provided with region annotations for each detection obtained using expert annotation from a biologist.',
+    BiocVersion = as.numeric(as.character(BiocManager::version())) + 0.01,
+    Genome = NA,
+    SourceType = 'CSV',
+    SourceUrl = 'https://nanostring.com/products/cosmx-spatial-molecular-imager/nsclc-ffpe-dataset',
+    SourceVersion = '2023-03-09',
+    Species = 'Homo sapiens',
+    TaxonomyId = '9606',
+    Coordinate_1_based = TRUE,
+    DataProvider = 'NanoString',
+    Maintainer = 'Dharmesh D. Bhuva <dharmesh.bhuva@adelaide.edu.au>',
+    RDataClass = 'data.frame',
+    DispatchClass = 'Rds',
+    RDataPath = file.path('SubcellularSpatialData', 'cosmx', 'cosmx_hs_nsclc.rds')
+  )
+)
+
+#write and test metadata file
+write.csv(meta, file = 'inst/extdata/metadata.csv', row.names = FALSE)
+ExperimentHubData::makeExperimentHubMetadata('../SubcellularSpatialData')
